f2d3e4a79c0587a9ffcb5122423a87a18a55c601
wanghao12345 <1750110604@qq.com>
2017-12-21 22:36:10 +0800

    up

---
+++ static/js/login/login.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	var telNo = getUrlParam('telNo');
+	$("#userName").val(telNo);
+	var backUrl = getUrlParam('backUrl');
+
+
+	$(".login_btn").on('click',function(){
+		var phone = $("#userName").val();
+		var pwd = $("#passWord").val();
+		loginGet(phone,pwd,backUrl);
+	})
+});
+
+//登录接口
+function loginGet(phone,pwd,backUrl){
+	var xmlhttp;
+	var url = "http://ateam.ticp.io:9107/36?phone="+phone+"&pwd="+pwd;
+	if (window.XMLHttpRequest){
+	  	xmlhttp=new XMLHttpRequest();
+	}
+	else{
+	  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function(){
+	  if (xmlhttp.readyState==4 && xmlhttp.status==200){
+	  		var obj = xmlhttp.responseText;
+	  		if (obj=="ok") {
+	  			window.location.href=backUrl;
+	  		}else{
+	  			alert("授权失败！");
+	  		}    
+	    }
+	}
+	xmlhttp.open("GET",url,true);
+	xmlhttp.send();
+}
+//获取url中的参数
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r != null) return unescape(r[2]); return null; //返回参数值
+}
